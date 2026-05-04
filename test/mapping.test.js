5b23a92b5c5d38ff8b47ab5bf4b69fcb9e99b92f
MarxLaw <marxbuison@gmail.com>
2026-05-04 14:23:16 +0800

    testttt

--- test/mapping.test.js
+++ test/mapping.test.js
@@ -1,10 +1,17 @@
 const request = require("supertest");
 const express = require("express");
-const mongoose = require("../database/mongo"); // this already connects
+const mongoose = require("../database/mongo");
 
 const app = express();
 app.use(express.json());
 app.use("/mapping", require("../routes/mapping"));
+
+beforeAll(async () => {
+  // wait until fully connected
+  if (mongoose.connection.readyState !== 1) {
+    await new Promise((resolve) => mongoose.connection.once("open", resolve));
+  }
+});
 
 afterAll(async () => {
   await mongoose.connection.close();
