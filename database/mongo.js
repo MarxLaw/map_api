d3b7092c9b4ecefa0e745888faf87b6c6759ea0f
MarxLaw <marxbuison@gmail.com>
2026-05-04 13:59:47 +0800

    test again

--- database/mongo.js
+++ database/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/mappng")
+  .connect("mongodb://localhost:27017/mapping")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
