d771ba53f55fda8f06f8756d51233fd312245012
MarxLaw <marxbuison@gmail.com>
2026-05-04 14:19:56 +0800

    test again r

--- test/mapping.test.js
+++ test/mapping.test.js
@@ -1,16 +1,10 @@
 const request = require("supertest");
 const express = require("express");
-const mongoose = require("mongoose");
+const mongoose = require("../database/mongo"); // this already connects
 
 const app = express();
 app.use(express.json());
 app.use("/mapping", require("../routes/mapping"));
-
-beforeAll(async () => {
-  await mongoose.connect(
-    process.env.MONGO_URI || "mongodb://127.0.0.1:27017/mapping",
-  );
-});
 
 afterAll(async () => {
   await mongoose.connection.close();
